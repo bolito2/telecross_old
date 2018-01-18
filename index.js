0fdac9c44e384d587f0646c36a99be225a201912
Bolito2 <bolito2hd@gmail.com>
2018-01-18 15:05:20 +0100

    primer commit real yee

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const PORT = process.env.PORT || 5000
 var app = express()
 
 app
+  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
